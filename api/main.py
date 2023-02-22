5d14b54043dd62f90f502abc48c354e8e0425a11
Alyn <alyndliang@gmail.com>
2023-02-21 17:53:38 -0800

    Alyn Best Branch

---
+++ api/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+import json
+
+app = FastAPI()
+
+# Load data from questions.json file
+with open('api/questions.json', 'r') as file:
+    contents = file.read()
+    data = json.loads(contents)
+
+# GET all questions
+@app.get("/questions")
+async def get_questions():
+    return data
+
+# GET a specific question by ID
+@app.get("/questions/{question_id}")
+async def get_question(question_id: str):
+    for question in data:
+        if question['id'] == question_id:
+            return question
+    return {"error": "Question not found"}
+
+# POST a new question
+@app.post("/questions")
+async def create_question(new_question: dict):
+    data.append(new_question)
+    with open('api/questions.json', 'w') as file:
+        file.write(json.dumps(data, indent=2))
+    return {"message": "Question created successfully"}
+
+# DELETE a question by ID
+@app.delete("/questions/{question_id}")
+async def delete_question(question_id: str):
+    global data
+    data = [question for question in data if question['id'] != question_id]
+    with open('api/questions.json', 'w') as file:
+        file.write(json.dumps(data, indent=2))
+    return {"message": "Question deleted successfully"}
