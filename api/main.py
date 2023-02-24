f5985765fcb58b9fd8184f418688cd80b9d7ed79
David <david.regalado92@gmail.com>
2023-02-24 13:25:23 -0700

    Updated migrations, finished database, and updated main.py

--- api/main.py
+++ api/main.py
@@ -25,39 +25,3 @@ app.add_middleware(
 
 app.include_router(authenticator.router)
 app.include_router(accounts.router)
-
-
-# # Load data from questions.json file
-# with open('data/wizquiz.sql', 'r') as file:
-#     contents = file.read()
-#     data = json.loads(contents)
-
-# # GET all questions
-# @app.get("/questions")
-# async def get_questions():
-#     return data
-
-# # GET a specific question by ID
-# @app.get("/questions/{question_id}")
-# async def get_question(question_id: str):
-#     for question in data:
-#         if question['id'] == question_id:
-#             return question
-#     return {"error": "Question not found"}
-
-# # POST a new question
-# @app.post("/questions")
-# async def create_question(new_question: dict):
-#     data.append(new_question)
-#     with open('data/wizquiz.sql', 'w') as file:
-#         file.write(json.dumps(data, indent=2))
-#     return {"message": "Question created successfully"}
-
-# # DELETE a question by ID
-# @app.delete("/questions/{question_id}")
-# async def delete_question(question_id: str):
-#     global data
-#     data = [question for question in data if question['id'] != question_id]
-#     with open('data/wizquiz.sql', 'w') as file:
-#         file.write(json.dumps(data, indent=2))
-#     return {"message": "Question deleted successfully"}
