36af7166505006b5b0891b4e644a486cba22f40c
David Regalado <david.regalado92@gmail.com>
2023-02-24 20:05:16 +0000

    Scorch branch

---
+++ user/migrations/001_create_first_tables.py
@@ -0,0 +1,36 @@
+steps = [
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY NOT NULL,
+            avatar_img() NOT NULL,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            username VARCHAR(255)  NOT NULL,
+            password TEXT NOT NULL
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE users;
+        """,
+    ]
+
+
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE superusers (
+            id SERIAL PRIMARY KEY NOT NULL,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            username VARCHAR(255)  NOT NULL,
+            password TEXT NOT NULL
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE users;
+        """,
+    ]
+
+]
