edb92fab6a018cde3d5a50b0d9b2cd558cec6bf3
Giovanni Garcia <giovannigarcia13128@gmail.com>
2023-03-07 20:23:01 +0000

    Gio2 branch

--- ghi/src/components/quiz.js
+++ ghi/src/components/quiz.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import "./quiz.css"
+import "./quiz.css";
 
 const Quiz = () => {
   const [questions, setQuestions] = useState([]);
