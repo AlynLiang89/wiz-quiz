5bdff4a86cb39912af5f02469ccbcbb6bd736d8c
Alyn <alyndliang@gmail.com>
2023-03-06 16:49:45 -0800

    stuff

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,20 +1,36 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { AuthProvider, useToken } from "./auth";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import LogInForm from "./Login";
+import SignupForm from "./Signup";
+import HomePage from "./homepage";
 import Quiz from "./components/quiz";
-import "./components/style.css";
-import MainPage from "./components/MainPage";
-import Leaderboard from "./components/leaderboard";
+import Leaderboard from "./leaderboard";
+import UpdateProfile from "./update";
+
+function GetToken() {
+  // Get token from cookie only if logged in
+  useToken();
+  return null;
+}
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/quiz" element={<Quiz />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-        </Routes>
+        <AuthProvider>
+          <GetToken />
+          <div className="container"></div>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LogInForm />} />
+            <Route path="/signup" element={<SignupForm />} />
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/update" element={<UpdateProfile />} />
+          </Routes>
+          <div />
+        </AuthProvider>
       </BrowserRouter>
     </div>
   );
