09765c273cf5f3579ce2e7cb8f3ac189b7e3ae34
Sean McKee <seanmichaelmckee@gmail.com>
2023-03-08 18:39:39 +0000

    Deploy branch

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -11,27 +11,33 @@ function GetToken() {
   useToken();
   return null;
 }
+const domain = /https:\/\/[^/]+/;
+const basename = process.env.PUBLIC_URL.replace(domain, "");
+return (
+  <BrowserRouter basename={basename}>
+    {function App() {
+      return (
+        <div>
+          <BrowserRouter>
+            <AuthProvider>
+              <GetToken />
+              <div className="container"></div>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LogInForm />} />
+                <Route path="/signup" element={<SignupForm />} />
+                <Route path="/quiz" element={<Quiz />} />
+                <Route path="/leaderboard" element={<Leaderboard />} />
+                <Route path="/update" element={<UpdateProfile />} />
+              </Routes>
+              <div />
+            </AuthProvider>
+          </BrowserRouter>
+        </div>
+      );
+    }}
+  </BrowserRouter>
+);
 
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <AuthProvider>
-          <GetToken />
-          <div className="container"></div>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LogInForm />} />
-            <Route path="/signup" element={<SignupForm />} />
-            <Route path="/quiz" element={<Quiz />} />
-            <Route path="/leaderboard" element={<Leaderboard />} />
-            <Route path="/update" element={<UpdateProfile />} />
-          </Routes>
-          <div />
-        </AuthProvider>
-      </BrowserRouter>
-    </div>
-  );
-}
 
 export default App;
