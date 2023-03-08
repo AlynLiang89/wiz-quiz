75872a90484ad58f21eb5889f25f2fae1ef44eb3
Sean <seanmichaelmckee@gmail.com>
2023-03-08 15:42:02 -0800

    working tests

--- api/tests/test_alyn.py
+++ api/tests/test_alyn.py
@@ -5,32 +5,32 @@ from queries.questions import QuestionQueries
 
 client = TestClient(app)
 
-
 class CreateQuestionQueries:
     def create(self, question):
         result = {
+
             "question": "string",
             "answer": "string",
             "option_1": "string",
             "option_2": "string",
-            "option_3": "string",
-        }
+            "option_3": "string"
+            }
 
         result.update(question)
         return result
-
 
 def test_question_create():
 
     app.dependency_overrides[QuestionQueries] = CreateQuestionQueries
 
     json = {
+
         "question": "Who voiced Batman in the 1992 Batman the animated series?",
         "answer": "Kevin Conroy",
         "option_1": "Peter Cullen",
         "option_2": "David Kayne",
-        "option_3": "Gary Chaulk",
-    }
+        "option_3": "Gary Chaulk"
+        }
 
     expected = {
         "id": None,
@@ -38,8 +38,9 @@ def test_question_create():
         "answer": "Kevin Conroy",
         "option_1": "Peter Cullen",
         "option_2": "David Kayne",
-        "option_3": "Gary Chaulk",
-    }
+        "option_3": "Gary Chaulk"
+        }
+
 
     response = client.post("/api/questions/", json=json)
 
